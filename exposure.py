3d2160cd719d4296883f6f9488b527b2441f8fad
신현준 <102020168+sjundo@users.noreply.github.com>
2023-10-05 16:07:32 +0900

    Create exposure.py

---
+++ exposure.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(0) 
+
+# 카메라 속성 설정 (노출 조절)
+exposure_value = -3  # 노출 값을 조정. 더 작은 값은 노출을 줄임
+cap.set(cv2.CAP_PROP_EXPOSURE, exposure_value)
+
+while True:
+    ret, frame = cap.read()
+    
+    cv2.imshow('Camera', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
